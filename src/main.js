4bbc0bc90531f03b8b4293831a4566efd5949e3b
JC-Aires <jcaires63@gmail.com>
2023-01-28 10:52:10 -0300

    Highlight Nav with Scroll

--- src/main.js
+++ src/main.js
@@ -1,1 +1,23 @@
 AOS.init();
+
+const sections = document.querySelectorAll('section');
+const navA = document.querySelectorAll('.menu__item a');
+
+window.addEventListener('scroll', () => {
+    let current = '';
+
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.clientHeight;
+        if (scrollY >= (sectionTop - sectionHeight / 5)) {
+            current = section.getAttribute('id');
+        }
+    })
+
+    navA.forEach(a => {
+        a.classList.remove('active');
+        if (a.classList.contains(current)) {
+            a.classList.add('active')
+        }
+    })
+})
